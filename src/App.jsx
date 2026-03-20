c359c7497ea29da8dcd447b4ba67cf34f25c927e
varshadinesg-alt <varshadineg@gmail.com>
2026-03-21 00:49:23 +0530

    initial commit: project structure and brand assets

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <h1 className={"text-5xl text-orange-500 font-bold font-circular-web"}>Welcome to Awwwards</h1>
+        </main>
+    )
+}
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ['zentry', 'sans-serif'],
+        general: ['general', 'sans-serif'],
+        'circular-web': ['circular-web', 'sans-serif'],
+        'robert-medium': ['robert-medium', 'sans-serif'],
+        'robert-regular': ['robert-regular', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
